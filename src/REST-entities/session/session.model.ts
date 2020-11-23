bca55966d4fc42c3571996c2b6ddb58d818fbdf3
DanTsvirkun <danila.tsvirkun555@gmail.com>
2020-11-23 04:07:45 +0200

    couple adjustments

--- src/REST-entities/session/session.model.ts
+++ src/REST-entities/session/session.model.ts
@@ -1,7 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 
 const sessionSchema = new Schema({
-  uid: { type: mongoose.Types.ObjectId, required: true },
+  uid: mongoose.Types.ObjectId,
 });
 
 export default mongoose.model("Session", sessionSchema);
